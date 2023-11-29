f2440ada38a65b07ee5b37bda508721460986af8
KunCheng-He <byack-hkc@foxmail.com>
2023-11-29 19:15:02 +0800

    数据库的配置

---
+++ callcenter_emo/config-demo.py
@@ -0,0 +1,9 @@
+# MySQL 配置项
+mysql_config = {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'xxx',
+    'USER': 'xxx',
+    'PASSWORD': 'xxx',
+    'HOST': 'xxx',
+    'PORT': 'xxx',
+}
--- callcenter_emo/settings.py
+++ callcenter_emo/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+from config import mysql_config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +27,7 @@ SECRET_KEY = 'django-insecure-mkld8i9b3okgj4p-8r!&kl)eqpt+%1j(kp&68r6ff_t_6rq$rz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',    # REST API
 ]
 
 MIDDLEWARE = [
@@ -74,10 +77,11 @@ WSGI_APPLICATION = 'callcenter_emo.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    'default': mysql_config
 }
 
 
@@ -103,13 +107,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
