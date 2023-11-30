fa1b7e9ee7cb75249e72f229343bc536a7c2deed
KunCheng-He <byack-hkc@foxmail.com>
2023-11-30 17:09:21 +0800

    角色增删改权限管理

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser
 
 from .models import UserRole
 from .serializers import UserRoleSerializer
@@ -11,3 +12,9 @@ class UserRoleViewSet(viewsets.ModelViewSet):
     """用户角色视图集"""
     queryset = UserRole.objects.all()
     serializer_class = UserRoleSerializer
+
+    def get_permissions(self):
+        """重写get_permissions方法，允许管理员增删改，查不受限制"""
+        if self.action in ["create", "update", "partial_update", "destroy"]:
+            return [IsAdminUser()]
+        return super().get_permissions()
