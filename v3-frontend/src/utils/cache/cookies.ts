82ae89cad39e5af8b6e46fb2e37479778928d12e
KunCheng He <byack-hkc@foxmail.com>
2023-12-25 11:03:43 +0800

    存储基本用户信息

--- v3-frontend/src/utils/cache/cookies.ts
+++ v3-frontend/src/utils/cache/cookies.ts
@@ -12,3 +12,15 @@ export const setToken = (token: string) => {
 export const removeToken = () => {
   Cookies.remove(CacheKey.TOKEN)
 }
+
+/** 统一处理 登录用户的基本信息 */
+const UserInfoKey = "userinfo"
+export const getUserInfo = () => {
+  return Cookies.get(JSON.parse(UserInfoKey))
+}
+export const setUserInfo = (token: object) => {
+  Cookies.set(UserInfoKey, JSON.stringify(token))
+}
+export const removeUserInfo = () => {
+  Cookies.remove(UserInfoKey)
+}
