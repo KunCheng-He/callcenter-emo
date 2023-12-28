c2139f26f725ebb3e633b3b2c87a85ae63ecb7a6
KunCheng He <byack-hkc@foxmail.com>
2023-12-28 11:01:06 +0800

    删除旧的前端模板

--- frontend/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import { constantRouterMap } from './router'
-
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: constantRouterMap
-})
-
-export default router
