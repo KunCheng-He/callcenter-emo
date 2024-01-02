db572bbc78030205683b2dadd5100b1ca1dcba3f
KunCheng He <byack-hkc@foxmail.com>
2024-01-02 22:45:25 +0800

    上传事件序列化

---
+++ apps/upload_events/serializers.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from .models import UploadEvent
+
+
+# 上传事件序列化
+class UploadEventSerializer(serializers.HyperlinkedModelSerializer):
+    """ 上传事件序列化 """
+    class Meta:
+        model = UploadEvent
+        fields = ['id', 'cs_user_id', 'upload_time', 'file']
+        read_only_fields = ['id', 'cs_user_id', 'upload_time']
+    
