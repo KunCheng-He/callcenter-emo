f5710699809d6378cde87f4eddd000496e93650c
KunCheng He <byack-hkc@foxmail.com>
2024-01-20 12:04:39 +0800

    情感识别结果统计到用户信息

--- cc_celery/tasks/tasks.py
+++ cc_celery/tasks/tasks.py
@@ -6,6 +6,7 @@ from .tools import get_upload_event_info, unzip_file, mp3_add_database
 from .ai_ser.recognition import recognize_sentiment
 from apps.audio.models import Audio
 from apps.emotion.models import Emotion
+from apps.accounts.models import CustomUser
 
 
 # 异步测试任务
@@ -31,7 +32,7 @@ def audio_process(upload_dict: dict):
         # 处理mp3格式的文件
         audio_id = mp3_add_database(event_id, file_full_path, cs_user_id)
         # 识别音频情感
-        emotion_recognition.delay(audio_id)
+        emotion_recognition.delay(audio_id, cs_user_id)
         return f"event_id:{event_id} audio add database, filename:{file_path}"
     elif file_type == '.zip':
         # 处理zip格式的文件
@@ -42,7 +43,7 @@ def audio_process(upload_dict: dict):
                     mp3_file_path = os.path.join(root, file)
                     audio_id = mp3_add_database(event_id, mp3_file_path, cs_user_id)
                     # 识别音频情感
-                    emotion_recognition.delay(audio_id)
+                    emotion_recognition.delay(audio_id, cs_user_id)
         return f"event_id:{event_id} zip file unzip and audio add database, zipname:{file_path}"
     else:
         # 其他格式的文件代表有误
@@ -51,7 +52,7 @@ def audio_process(upload_dict: dict):
 
 
 @app.task
-def emotion_recognition(audio_id: int):
+def emotion_recognition(audio_id: int, user_id: int):
     """
     情感识别任务，根据给定的音频id进行情感识别，并将结果写入情感数据库
     """
@@ -71,4 +72,13 @@ def emotion_recognition(audio_id: int):
         right_emotions = right_emotions
     )
     emotion_object.save()
+    # 统计情感信息更新到客服的用户信息中
+    user_object = CustomUser.objects.get(id=user_id)
+    user_object.user_emo_up = user_object.user_emo_up + left_emotions.count(0)
+    user_object.user_emo_norm = user_object.user_emo_norm + left_emotions.count(1)
+    user_object.user_emo_down = user_object.user_emo_down + left_emotions.count(2)
+    user_object.emo_up = user_object.emo_up + right_emotions.count(0)
+    user_object.emo_norm = user_object.emo_norm + right_emotions.count(1)
+    user_object.emo_down = user_object.emo_down + right_emotions.count(2)
+    user_object.save()
     return f"audio_id:{audio_id} emotion recognition success, add to emotion database, emotion_id:{emotion_object.id}"
