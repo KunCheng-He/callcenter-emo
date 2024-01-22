42fc83e1b7b9e911e2b3fe09a34918614463a01b
KunCheng He <byack-hkc@foxmail.com>
2024-01-22 17:54:32 +0800

    音频审阅查询集

--- apps/audio/views.py
+++ apps/audio/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from rest_framework import viewsets
 
 from .models import Audio
@@ -12,3 +12,14 @@ class AudioViewSet(viewsets.ModelViewSet):
     queryset = Audio.objects.all()
     serializer_class = AudioSerializer
     http_method_names = ['get']
+
+    def get_queryset(self):
+        checked = self.request.query_params.get('checked', None)
+        
+        # 如果用户提供了 checked 参数，过滤出未审计的样本
+        if checked == "True":
+            return Audio.objects.filter(checked=True)
+        elif checked == "False":
+            return Audio.objects.filter(checked=False)
+        else:
+            return Audio.objects.all()
