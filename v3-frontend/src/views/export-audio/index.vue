7ac83612421fb70daabc598f71f2923bc29181e1
KunCheng-He <byack-hkc@foxmail.com>
2024-04-10 17:57:41 +0800

    导出音频页面大概

--- v3-frontend/src/views/export-audio/index.vue
+++ v3-frontend/src/views/export-audio/index.vue
@@ -1,4 +1,24 @@
 <script lang="ts" setup>
+import { ref } from "vue"
+import { ElCard, ElForm, ElFormItem, ElDatePicker, ElButton, ElMessage } from "element-plus"
+
+const exportForm = ref({
+  startTime: "",
+  endTime: ""
+})
+
+const handleExport = async () => {
+  // 模拟导出功能
+  try {
+    // 模拟请求后端导出数据
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    ElMessage.success("语料库正在导出，请稍候...")
+  } catch (error) {
+    ElMessage.error("语料库导出失败，请重试...")
+  }
+}
+
 defineOptions({
   name: "ExportAudio"
 })
@@ -6,10 +26,43 @@ defineOptions({
 
 <template>
   <div class="app-container">
-    <el-card shadow="never">
-      <h1>Export-Audio</h1>
+    <el-card class="export-card">
+      <h3 class="title">语料库导出</h3>
+      <el-form :model="exportForm" inline>
+        <el-form-item label="开始时间">
+          <el-date-picker v-model="exportForm.startTime" type="date" placeholder="选择开始时间" />
+        </el-form-item>
+        <el-form-item label="结束时间">
+          <el-date-picker v-model="exportForm.endTime" type="date" placeholder="选择结束时间" />
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="handleExport">导出</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.export-card {
+  margin-top: 20px;
+}
+
+.title {
+  font-size: 1rem;
+  line-height: 1.5rem;
+  --tw-text-opacity: 1;
+  color: rgb(148 163 184 / var(--tw-text-opacity));
+  margin-bottom: 15px;
+  margin-top: -10px;
+}
+
+.card-title {
+  position: absolute;
+  top: 10px;
+  left: 10px;
+  font-weight: bold;
+  font-size: 18px;
+  color: #333;
+}
+</style>
