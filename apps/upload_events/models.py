dc7656aa400b713eaed8803009e217c1da0c4ed3
KunCheng-He <byack-hkc@foxmail.com>
2024-04-20 16:23:52 +0800

    上传事件表添加模型外键

--- apps/upload_events/models.py
+++ apps/upload_events/models.py
@@ -1,5 +1,6 @@
 from django.db.models import Model, ForeignKey, SET_NULL, DateTimeField, FileField
 from apps.accounts.models import CustomUser
+from apps.ser_model.models import SERModel
 
 # Create your models here.
 
@@ -7,5 +8,6 @@ from apps.accounts.models import CustomUser
 class UploadEvent(Model):
     """ 上传事件表 """
     cs_user_id = ForeignKey(CustomUser, on_delete=SET_NULL, null=True, related_name='cs_events', verbose_name='客服用户')
+    ser_model_id = ForeignKey(SERModel, on_delete=SET_NULL, null=True, related_name='ser_model', verbose_name='语音情感识别模型')
     upload_time = DateTimeField(auto_now_add=True, verbose_name='上传时间')
     file = FileField(upload_to='upload_files/%Y/%m/%d', verbose_name='上传文件')
--- apps/upload_events/serializers.py
+++ apps/upload_events/serializers.py
@@ -7,6 +7,6 @@ class UploadEventSerializer(serializers.HyperlinkedModelSerializer):
     """ 上传事件序列化 """
     class Meta:
         model = UploadEvent
-        fields = ['url', 'cs_user_id', 'upload_time', 'file']
+        fields = ['url', 'cs_user_id', 'ser_model_id', 'upload_time', 'file']
         read_only_fields = ['upload_time',]
     
